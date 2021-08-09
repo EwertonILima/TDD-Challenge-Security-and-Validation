720280aebddc92083a90955ffbf648e33cf11d6f
EwertonILima <ewertoninaciolima@gmail.com>
2021-08-09 19:11:34 -0300

    City, Event insert

--- src/main/java/com/devsuperior/bds04/services/EventService.java
+++ src/main/java/com/devsuperior/bds04/services/EventService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.entities.City;
 import com.devsuperior.bds04.entities.Event;
 import com.devsuperior.bds04.repositories.EventRepository;
 
@@ -20,5 +21,16 @@ public class EventService {
 	public Page<EventDTO> findAllPaged(Pageable pageable) {
 		Page<Event> page = eventRepository.findAll(pageable);
 		return page.map(x -> new EventDTO(x));
+	}
+
+	@Transactional
+	public EventDTO insert(EventDTO dto) {
+		Event entity = new Event();
+		entity.setName(dto.getName());
+		entity.setDate(dto.getDate());
+		entity.setUrl(dto.getUrl());
+		entity.setCity(new City(dto.getCityId(), null));
+		entity = eventRepository.save(entity);
+		return new EventDTO(entity);
 	}
 }
